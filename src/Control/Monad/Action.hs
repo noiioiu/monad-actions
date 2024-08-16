0227b9f731f9997cec2c6d8f5cd1e00ac16885a3
noiioiu <noiioiu@cocaine.ninja>
2024-08-16 09:31:10 -0500

    Add bimodule class

--- src/Control/Monad/Action.hs
+++ src/Control/Monad/Action.hs
@@ -3,6 +3,7 @@ module Control.Monad.Action
   ( module Control.Monad,
     LeftModule (..),
     RightModule (..),
+    BiModule (..),
   )
 where
 
@@ -13,19 +14,22 @@ import Control.Monad
 -- * @'lscale' '.' 'join' = 'lscale' '.' 'fmap' 'lscale'@
 --
 -- * @'lscale' . 'return' = 'id'@
-class Monad m => LeftModule f m where
+class (Monad m) => LeftModule m f where
   lscale ::
-    m (f a) ->
-    -- | left "scalar multiplication"
-    f a
+    m (f a) -> f a -- ^ left "scalar multiplication"
 
 -- | Instances must satisfy the following laws:
 --
 -- * @'rscale' . 'fmap' 'join' = 'rscale' . 'rscale'@
 --
 -- * @'rscale' . 'fmap' 'return' = 'id'@
-class Monad m => RightModule f m where
+class (Monad m) => RightModule m f where
   rscale ::
-    f (m a) ->
-    -- | right "scalar multiplication"
-    f a
+    f (m a) -> f a -- ^ right "scalar multiplication"
+
+-- | Given two monads r and s, an (r, s) bimodule is a functor that is a left module over r and a right module over s, where the two "scalar multiplications" are compatible.
+--   Instances must satisfy the following law in addition to the laws for @'LeftModule'@ and @'RightModule'@:
+--
+-- * @'rscale' . 'lscale' = 'lscale' . 'fmap' 'rscale' = 'biscale'@
+class (LeftModule r f, RightModule s f) => BiModule r s f where
+  biscale :: r (f (s a)) -> f a -- ^ two-sided "scalar multiplication"
