d26355917cfe86a8d61959b2c00e4de0b0e6569f
noiioiu <noiioiu@cocaine.ninja>
2024-09-13 17:41:06 -0500

    add join to qualified do modules

--- src/Control/Monad/Action/Right.hs
+++ src/Control/Monad/Action/Right.hs
@@ -1,10 +1,16 @@
-module Control.Monad.Action.Right ((>>=), (>>), pure, return, fail) where
+module Control.Monad.Action.Right ((>>=), (>>), pure, return, fail, join) where
 
 import Control.Monad.Action
-import Prelude hiding ((>>), (>>=))
+import Prelude hiding (return, (>>), (>>=))
 
 (>>=) :: (RightModule m f) => f a -> (a -> m b) -> f b
 (>>=) = (ract .) . flip fmap
 
 (>>) :: (RightModule m f) => f a -> m b -> f b
 (>>) = (ract .) . flip (fmap . const)
+
+return :: (Applicative f) => a -> f a
+return = pure
+
+join :: (RightModule m f) => f (m a) -> f a
+join = ract
