94fb23eb28a5b07b0ea7a6f4c365f48305f7fbce
noiioiu <noiioiu@cocaine.ninja>
2024-09-28 22:40:31 -0500

    add instances for NonEmpty

--- src/Control/Monad/Action/Right.hs
+++ src/Control/Monad/Action/Right.hs
@@ -1,13 +1,22 @@
-module Control.Monad.Action.Right ((>>=), (>>), pure, return, fail, join) where
+module Control.Monad.Action.Right ((>>=), (>>), (=<<), (>=>), (<=<), pure, return, fail, join) where
 
 import Control.Monad.Action
-import Prelude hiding (return, (>>), (>>=))
+import Prelude hiding (return, (=<<), (>>), (>>=))
 
 (>>=) :: (RightModule m f) => f a -> (a -> m b) -> f b
 (>>=) = (rjoin .) . flip fmap
 
+(=<<) :: (RightModule m f) => (a -> m b) -> f a -> f b
+(=<<) = (rjoin .) . fmap
+
 (>>) :: (RightModule m f) => f a -> m b -> f b
 (>>) = (rjoin .) . flip (fmap . const)
+
+(>=>) :: (RightModule m f) => (a -> f b) -> (b -> m c) -> a -> f c
+(>=>) = flip $ (.) . (=<<)
+
+(<=<) :: (RightModule m f) => (b -> m c) -> (a -> f b) -> a -> f c
+(<=<) = (.) . (=<<)
 
 return :: (Applicative f) => a -> f a
 return = pure
