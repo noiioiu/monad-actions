d2b30110da7d38d593604cba91628cb48acace5c
noiioiu <noiioiu@cocaine.ninja>
2024-10-27 08:52:04 -0500

    Update documentation

--- src/Control/Monad/Action.hs
+++ src/Control/Monad/Action.hs
@@ -1,4 +1,23 @@
--- | A monad action is a monoid action in the category of endofunctors, what's the problem?
+-- | Given a monad \(M\) on a category \(\mathcal{D}\) with unit \(\eta\) and
+--     multiplication \(\mu\) and a functor \(F\) from \(\mathcal{C}\) to \(\mathcal{D}\),
+--     a left monad action of \(M\) on \(F\) is a natural transformation \(\nu\) such that
+--     the following two laws hold:
+--
+--     * \(\nu \cdot (\eta \circ F) = \mathrm{id}_F\)
+--     * \(\nu \cdot (\mu \circ F) = \nu \cdot (M \circ \nu)\)
+--
+--     We also say that \(F\) is a left module over \(M\).  In the case
+--     \(\mathcal{C} = \mathcal{D}\), a left monad module is a left monoid module
+--     object in the category of endofunctors on \(\mathcal{C}\).  We may also
+--     call \(\alpha\) the scalar multiplication of the module by the monad, by analogy
+--     with ring modules, which are monoid module objects in the category of abelian groups
+--     with tensor product as the monoidal product (rings are just monoid objects in this
+--     category).
+--
+--     Right monad actions are defined similarly.
+--
+--     See [this blog post](https://stringdiagram.com/2023/04/23/monad-actions/) by Dan Marsden
+--     or the paper /Modules over monads and their algebras/ by Piróg, Wu, and Gibbons.
 
 {-# LANGUAGE IncoherentInstances #-}
 
