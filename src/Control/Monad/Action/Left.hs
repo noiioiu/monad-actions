d85dbf08195c4e060008ba73cec6cf16f3f44ad6
noiioiu <noiioiu@cocaine.ninja>
2025-12-26 20:05:09 -0600

    Add applicative methods and fixities to Left

--- src/Control/Monad/Action/Left.hs
+++ src/Control/Monad/Action/Left.hs
@@ -1,20 +1,30 @@
 -- | This module is meant to be used with the @QualifiedDo@ extension.
-module Control.Monad.Action.Left ((>>=), (>>), (=<<), (>=>), (<=<), pure, return, fail, join) where
+module Control.Monad.Action.Left ((>>=), (>>), (=<<), (>=>), (<=<), (<*>), fmap, pure, return, fail, join) where
 
 import Control.Monad.Action
-import Prelude hiding (return, (=<<), (>>), (>>=))
+import Prelude hiding (return, (<*>), (=<<), (>>), (>>=))
+
+infixl 1 >>=
 
 (>>=) :: (LeftModule m f) => m a -> (a -> f b) -> f b
 (>>=) = (ljoin .) . flip fmap
 
+infixr 1 =<<
+
 (=<<) :: (LeftModule m f) => (a -> f b) -> m a -> f b
 (=<<) = (ljoin .) . fmap
+
+infixl 1 >>
 
 (>>) :: (LeftModule m f) => m a -> f b -> f b
 (>>) = (ljoin .) . flip (fmap . const)
 
+infixr 1 >=>
+
 (>=>) :: (LeftModule m f) => (a -> m b) -> (b -> f c) -> a -> f c
 (>=>) = flip $ (.) . (=<<)
+
+infixr 1 <=<
 
 (<=<) :: (LeftModule m f) => (b -> f c) -> (a -> m b) -> a -> f c
 (<=<) = (.) . (=<<)
@@ -24,3 +34,8 @@ return = pure
 
 join :: (LeftModule m f) => m (f a) -> f a
 join = ljoin
+
+infixl 4 <*>
+
+(<*>) :: (LeftModule m f) => m (a -> b) -> f a -> f b
+fs <*> xs = fs >>= flip fmap xs
